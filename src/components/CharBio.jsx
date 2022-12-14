b76fcdfd7948e87bdd1108220f8267456a343c1e
Raul Menendez <orreryofcode@gmail.com>
2022-12-14 14:34:35 -0800

    update and render bios in characterdata directory

--- src/components/CharBio.jsx
+++ src/components/CharBio.jsx
@@ -2,16 +2,8 @@ function CharBio({ charDetails }) {
   return (
     <div className='charInfo__bio-wrapper'>
       <div className='charInfo__bio'>
-        <h4>Bio:</h4>
-        <p className='charInfo__bio-details'>
-          Jin Kazama is the son of Jun Kazama and Kazuya Mishima. During Jun's
-          pregnancy, part of Kazuya's Devil, (which fled Kazuya when he was
-          thrown into a volcano by his father) attempted to possess Jin, who
-          inherited his father's Devil Gene. Jun managed to fight Devil off and
-          hold it at bay throughout her pregnancy and Jin's childhood. Jun
-          raised Jin in a remote location in the mountains of Yakushima, and
-          trained him in the Kazama family's self-defense fighting style.
-        </p>
+        <h4>Biography:</h4>
+        <p className='charInfo__bio-details'>{charDetails[0].bio}</p>
       </div>
 
       <div className='charInfo__demographics'>

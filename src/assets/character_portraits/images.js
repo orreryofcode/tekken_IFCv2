287351d64f5ae091677bc6aa6bbcdc8fe6a0797b
Raul Menendez <orreryofcode@gmail.com>
2022-12-16 15:05:18 -0800

    complete json data and page render logic for all characters

--- src/assets/character_portraits/images.js
+++ src/assets/character_portraits/images.js
@@ -2,5 +2,102 @@ import jin from "../character_portraits/jinkazama.svg";
 import bryan from "../character_portraits/bryan.svg";
 import king from "../character_portraits/king.svg";
 import nina from "../character_portraits/nina.svg";
+import ak from "../character_portraits/ak.svg";
+import akuma from "../character_portraits/akuma.svg";
+import alisa from "../character_portraits/alisa.svg";
+import anna from "../character_portraits/anna.svg";
+import asuka from "../character_portraits/asuka.svg";
+import bob from "../character_portraits/bob.svg";
+import chloe from "../character_portraits/chloe.svg";
+import claudio from "../character_portraits/claudio.svg";
+import dragunov from "../character_portraits/dragunov.svg";
+import dvj from "../character_portraits/dvj.svg";
+import eddy from "../character_portraits/eddy.svg";
+import eliza from "../character_portraits/eliza.svg";
+import fahkumram from "../character_portraits/fahkumram.svg";
+import feng from "../character_portraits/feng.svg";
+import ganryu from "../character_portraits/ganryu.svg";
+import geese from "../character_portraits/geese.svg";
+import gigas from "../character_portraits/gigas.svg";
+import heihachi from "../character_portraits/heihachi.svg";
+import hwoarang from "../character_portraits/hwoarang.svg";
+import jack from "../character_portraits/jack.svg";
+import josie from "../character_portraits/josie.svg";
+import julia from "../character_portraits/julia.svg";
+import katarina from "../character_portraits/katarina.svg";
+import kazumi from "../character_portraits/kazumi.svg";
+import kazuya from "../character_portraits/kazuya.svg";
+import kuma from "../character_portraits/kuma.svg";
+import kunimitsu from "../character_portraits/kunimitsu.svg";
+import lars from "../character_portraits/lars.svg";
+import law from "../character_portraits/law.svg";
+import lee from "../character_portraits/lee.svg";
+import lei from "../character_portraits/lei.svg";
+import leo from "../character_portraits/leo.svg";
+import leroy from "../character_portraits/leroy.svg";
+import lidia from "../character_portraits/lidia.svg";
+import lili from "../character_portraits/lili.svg";
+import marduk from "../character_portraits/marduk.svg";
+import miguel from "../character_portraits/miguel.svg";
+import negan from "../character_portraits/negan.svg";
+import noctis from "../character_portraits/noctis.svg";
+import paul from "../character_portraits/paul.svg";
+import raven from "../character_portraits/raven.svg";
+import shaheen from "../character_portraits/shaheen.svg";
+import steve from "../character_portraits/steve.svg";
+import xiaoyu from "../character_portraits/xiaoyu.svg";
+import yoshimitsu from "../character_portraits/yoshimitsu.svg";
+import zafina from "../character_portraits/zafina.svg";
 
-export { jin, bryan, king, nina };
+export {
+  jin,
+  bryan,
+  king,
+  nina,
+  ak,
+  akuma,
+  alisa,
+  anna,
+  asuka,
+  bob,
+  chloe,
+  claudio,
+  dragunov,
+  dvj,
+  eddy,
+  eliza,
+  fahkumram,
+  feng,
+  ganryu,
+  geese,
+  gigas,
+  heihachi,
+  hwoarang,
+  jack,
+  josie,
+  julia,
+  katarina,
+  kazumi,
+  kazuya,
+  kuma,
+  kunimitsu,
+  lars,
+  law,
+  lee,
+  lei,
+  leo,
+  leroy,
+  lidia,
+  lili,
+  marduk,
+  miguel,
+  negan,
+  noctis,
+  paul,
+  raven,
+  shaheen,
+  steve,
+  xiaoyu,
+  yoshimitsu,
+  zafina,
+};
